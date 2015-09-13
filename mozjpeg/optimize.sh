dda2dcb74266d109cfcc80a4f710d0205f1c0444
Johannes Holzfuß <DataWraith@fastmail.fm>
2015-09-13 08:27:43 +0200

    Add a script using the mozjpeg container to optimize JPEGs

---
+++ mozjpeg/optimize.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# This script takes a single JPEG file as argument and optimizes it, overwriting
+# the input image.
+
+if [ $# -ne 1 ]
+then
+  echo "Expected exactly one argument."
+  exit
+fi
+
+CANONICAL=$(readlink -e "$1")
+TMPFILE=$(mktemp)
+DIR=$(dirname "$CANONICAL")
+BASE=$(basename "$CANONICAL")
+
+docker run -v "$DIR":/images datawraith/mozjpeg -copy none -arithmetic "$BASE" > $TMPFILE \
+  && mv -f "$TMPFILE" "$1"
